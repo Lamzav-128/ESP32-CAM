1e35673764cbdec9b6d9d4c1bf79ed19d046ede6
Lamzav <108646239+Lamzav-128@users.noreply.github.com>
2023-05-25 17:35:13 +0800

    Update CameraWebServer.ino

--- CameraWebServer.ino
+++ CameraWebServer.ino
@@ -1,9 +1,9 @@
 #include "esp_camera.h"
 #include <WiFi.h>
-#define CAMERA_MODEL_AI_THINKER // Has PSRAM
+#define CAMERA_MODEL_AI_THINKER 
 #include "camera_pins.h"
-const char* ssid = "Engineering_Department";
-const char* password = "Engineering_Department_2021";
+const char* ssid = "****************************"; // ooriin wifi iin neriig oruulah
+const char* password = "***************************"; // wifi iin code oruulj ogoh
 void startCameraServer();
 void setupLedFlash(int pin);
 void setup() {
@@ -32,7 +32,6 @@ void setup() {
   config.xclk_freq_hz = 20000000;
   config.frame_size = FRAMESIZE_UXGA;
   config.pixel_format = PIXFORMAT_JPEG; 
-  //config.pixel_format = PIXFORMAT_RGB565; 
   config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
   config.fb_location = CAMERA_FB_IN_PSRAM;
   config.jpeg_quality = 12;
@@ -43,72 +42,56 @@ void setup() {
       config.fb_count = 2;
       config.grab_mode = CAMERA_GRAB_LATEST;
     } else {
-      // Limit the frame size when PSRAM is not available
       config.frame_size = FRAMESIZE_SVGA;
       config.fb_location = CAMERA_FB_IN_DRAM;
     }
   } else {
-    // Best option for face detection/recognition
     config.frame_size = FRAMESIZE_240X240;
 #if CONFIG_IDF_TARGET_ESP32S3
     config.fb_count = 2;
 #endif
   }
-
 #if defined(CAMERA_MODEL_ESP_EYE)
   pinMode(13, INPUT_PULLUP);
   pinMode(14, INPUT_PULLUP);
 #endif
-
-  // camera init
   esp_err_t err = esp_camera_init(&config);
   if (err != ESP_OK) {
     Serial.printf("Camera init failed with error 0x%x", err);
     return;
   }
-
   sensor_t * s = esp_camera_sensor_get();
   if (s->id.PID == OV3660_PID) {
     s->set_vflip(s, 1); 
     s->set_brightness(s, 1); 
     s->set_saturation(s, -2); 
   }
-
   if(config.pixel_format == PIXFORMAT_JPEG){
     s->set_framesize(s, FRAMESIZE_QVGA);
   }
-
 #if defined(CAMERA_MODEL_M5STACK_WIDE) || defined(CAMERA_MODEL_M5STACK_ESP32CAM)
   s->set_vflip(s, 1);
   s->set_hmirror(s, 1);
 #endif
-
 #if defined(CAMERA_MODEL_ESP32S3_EYE)
   s->set_vflip(s, 1);
 #endif
-
 #if defined(LED_GPIO_NUM)
   setupLedFlash(LED_GPIO_NUM);
 #endif
-
   WiFi.begin(ssid, password);
   WiFi.setSleep(false);
-
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
   Serial.println("");
   Serial.println("WiFi connected");
-
   startCameraServer();
-
   Serial.print("Camera Ready! Use 'http://");
   Serial.print(WiFi.localIP());
   Serial.println("' to connect");
 }
-
 void loop() {
-  
   delay(10000);
 }
